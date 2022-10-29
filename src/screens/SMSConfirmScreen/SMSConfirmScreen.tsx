48252c6f871886a63d8c41e8d2ca74a26da1fec8
Алексей Остриков <aleksey.ostrikov@instamart.ru>
2022-10-29 18:11:38 +0600

    navigation and styled-component

--- src/screens/SMSConfirmScreen/SMSConfirmScreen.tsx
+++ src/screens/SMSConfirmScreen/SMSConfirmScreen.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {Block} from '../../UIKit';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {AuthStackParamList} from '../../navigators/navigationTypes';
+import {EScreens} from '../../navigators';
+import {Button} from 'react-native';
 
-export const SMSConfirmScreen = () => {
+type Props = NativeStackScreenProps<
+  AuthStackParamList,
+  EScreens.SMS_CONFIRM_SCREEN
+>;
+
+export const SMSConfirmScreen: React.FC<Props> = ({navigation}) => {
   return (
     <Block
       backgroundColor={'blue'}
       flex={1}
       justifyContent={'center'}
-      padding={32}
-    />
+      padding={32}>
+      <Button title={'BACk'} onPress={() => navigation.goBack()} />
+    </Block>
   );
 };
