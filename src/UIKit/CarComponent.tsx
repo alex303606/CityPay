e40f8a63f5c5fe0664a0ca02a3610f2853b40888
Алексей Остриков <aleksey.ostrikov@instamart.ru>
2023-01-17 18:11:49 +0200

    delete car modal

--- src/UIKit/CarComponent.tsx
+++ src/UIKit/CarComponent.tsx
@@ -15,16 +15,18 @@ export const CarComponent: React.FC<Props> = ({number, onPress}) => {
     onPress && onPress(number);
   }, [number, onPress]);
 
+  const Button = onPress ? StyledPressable : Row;
+
   return (
     <StyledRow backgroundColor={Colors.white}>
-      <StyledPressable onPress={onPressHandler}>
+      <Button flex={1} onPress={onPressHandler}>
         <StyledImage source={flag} resizeMode={'cover'} />
         <Row justifyContent={'center'} flex={1} alignItems={'center'}>
           <Typography.B56 textAlign={'center'} color={Colors.black}>
             {number}
           </Typography.B56>
         </Row>
-      </StyledPressable>
+      </Button>
     </StyledRow>
   );
 };
@@ -36,7 +38,6 @@ const StyledPressable = styled(Pressable).attrs(() => ({
   },
 }))({
   flexDirection: 'row',
-  height: 65,
   flex: 1,
 });
 
