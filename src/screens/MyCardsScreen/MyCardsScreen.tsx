57099754089a9ab60565df9b7d57f5665d8af22c
Алексей Остриков <aleksey.ostrikov@instamart.ru>
2023-04-06 21:37:08 +0200

    cards

--- src/screens/MyCardsScreen/MyCardsScreen.tsx
+++ src/screens/MyCardsScreen/MyCardsScreen.tsx
@@ -1,11 +1,19 @@
-import React, {useCallback, useEffect} from 'react';
-import {Block, Button, ScreenContainer} from '@UIKit';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Block, Button, ScreenContainer, Typography} from '@UIKit';
 import {useTranslation} from 'react-i18next';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {EScreens, ProfileStackParamList} from '@navigators';
-import {NativeModules, NativeEventEmitter} from 'react-native';
-import {useAppSelector} from '@hooks';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  FlatList,
+  View,
+  ListRenderItem,
+} from 'react-native';
+import {useAppSelector, useSnackbarNotification, useTheme} from '@hooks';
 import {getUserState} from '@store';
+import {FlatListType} from '../types';
+import styled from 'styled-components';
 
 const {PayBoxModule} = NativeModules;
 
@@ -13,6 +21,14 @@ type Props = NativeStackScreenProps<
   ProfileStackParamList,
   EScreens.MY_CARDS_SCREEN
 >;
+
+type ICard = {
+  id: number;
+  number: string;
+};
+
+const keyExtractor = (item: ICard) => String(item.id);
+
 const payBoxModuleGetCards = (userId: string) => PayBoxModule.getCards(userId);
 const payBoxModuleAddCard = (userId: string) => {
   const postUrl = 'https://citysoft.kido.kg/';
@@ -22,6 +38,10 @@ const payBoxModuleAddCard = (userId: string) => {
 export const MyCardsScreen: React.FC<Props> = () => {
   const {t} = useTranslation();
   const {userId} = useAppSelector(getUserState);
+  const {theme} = useTheme();
+  const {showNotification} = useSnackbarNotification();
+
+  const [cards, setCards] = useState<ICard[]>([]);
 
   useEffect(() => {
     PayBoxModule.registerPbListener();
@@ -29,10 +49,16 @@ export const MyCardsScreen: React.FC<Props> = () => {
     const subscription = eventEmitter.addListener(
       'EventReminder',
       (event: Object) => {
-        const eventName = Object.keys(event)[0];
-        console.log(Object.values(event)[0]);
-        if (eventName === 'cardsList') {
-          console.log(eventName, JSON.parse(Object.values(event)[0]));
+        try {
+          const eventName = Object.keys(event)[0];
+          console.log(eventName, Object.values(event)[0]);
+          if (eventName === 'cardsList') {
+            console.log(eventName, JSON.parse(Object.values(event)[0]));
+            const cardsList = JSON.parse(Object.values(event)[0]);
+            setCards(cardsList);
+          }
+        } catch (e) {
+          return showNotification(t('errors.somethingWentWrong'));
         }
       },
     );
@@ -54,11 +80,32 @@ export const MyCardsScreen: React.FC<Props> = () => {
     }
   }, [userId]);
 
+  const renderItem: ListRenderItem<ICard> = useCallback(({item}) => {
+    return (
+      <Typography.S16 color={theme.textColor}>{item.number}</Typography.S16>
+    );
+  }, []);
+
   return (
-    <ScreenContainer title={t('profile.myCards')}>
+    <ScreenContainer scroll={false} title={t('profile.myCards')}>
       <Block flex={1}>
-        <Button title={t('profile.addCard')} onPress={addCardHandler} />
+        <List
+          showsVerticalScrollIndicator={false}
+          keyExtractor={keyExtractor}
+          data={cards}
+          renderItem={renderItem}
+          ItemSeparatorComponent={Separator}
+        />
       </Block>
+      <Button title={t('profile.addCard')} onPress={addCardHandler} />
     </ScreenContainer>
   );
 };
+
+const List: FlatListType = styled(FlatList).attrs(() => ({
+  contentContainerStyle: {
+    flexGrow: 1,
+  },
+}))({});
+
+const Separator = styled(View)({height: 16});
