b17155eab628076864aa37482a3c042778ed84d8
aleksey.ostrikov <ostrikov.as@sbermarket.ru>
2024-01-28 13:14:59 +0200

    ImageViewer

--- src/screens/Osago/PolicyScreen.tsx
+++ src/screens/Osago/PolicyScreen.tsx
@@ -1,11 +1,11 @@
 import React, {useCallback} from 'react';
-import {IconNames, ScreenContainer} from '@UIKit';
+import {Colors, IconNames, ScreenContainer, WINDOW_WIDTH} from '@UIKit';
 import {useTranslation} from 'react-i18next';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {EScreens, OsagoStackParamList} from '@navigators';
-import styled from 'styled-components';
-import {Image} from 'react-native';
+import {ActivityIndicator} from 'react-native';
 import Share from 'react-native-share';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 type Props = NativeStackScreenProps<
   OsagoStackParamList,
@@ -38,11 +38,16 @@ export const PolicyScreen: React.FC<Props> = ({route}) => {
       onPressButton={onPressShare}
       disablePaddings
       title={t('osago.policyScreen.title')}>
-      <StyledImage resizeMode={'contain'} source={{uri: url}} />
+      <ImageViewer
+        saveToLocalByLongPress={false}
+        style={{
+          width: WINDOW_WIDTH,
+          flex: 1,
+        }}
+        backgroundColor={Colors.white}
+        loadingRender={() => <ActivityIndicator size="large" color={'white'} />}
+        imageUrls={[{url: url}]}
+      />
     </ScreenContainer>
   );
 };
-
-const StyledImage = styled(Image)({
-  flex: 1,
-});
